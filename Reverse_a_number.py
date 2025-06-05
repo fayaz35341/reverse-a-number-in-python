eb82b31821484a43ede39289709c4d848994bb7e
Riyaz shaik <94755295+fayaz35341@users.noreply.github.com>
2025-06-05 22:46:28 +0530

    Update Reverse_a_number.py

--- Reverse_a_number.py
+++ Reverse_a_number.py
@@ -1,6 +1,33 @@
+# class Solution:
+#     def reverseNumber(self, n):
+#         a=list(str(n))
+#         b=(a[::-1])
+#         return int(''.join(map(str, b)))
+# print(Solution().reverseNumber(74))
+
+# python
 class Solution:
     def reverseNumber(self, n):
-        a=list(str(n))
-        b=(a[::-1])
-        return int(''.join(map(str, b)))
-print(Solution().reverseNumber(74))
+        reverse_num=0
+        while n>0:
+            ld=int(n%10)
+            reverse_num=int(reverse_num*10)+ld
+            n=int(n/10)
+        return reverse_num
+print(Solution().reverseNumber(745497))
+
+# JS
+
+class Solution {
+    reverseDigits(n) {
+        // code here
+        let reverse_num=0
+        while (n>0){
+            let ld=(n%10)
+            reverse_num=(reverse_num*10)+ld
+            n=Math.floor(n/10)
+        }
+        return reverse_num
+    }
+}
+console.log(new Solution().reverseDigits(123))
